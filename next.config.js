8bf33b7e49789a60acd3d0499b318e16125f7c93
devarsh-mavani-19 <devarshmavani19@gmail.com>
2021-11-10 00:55:49 +0530

    Initial Commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['localhost', 'icon-library.com', 'image.tmdb.org']
+    domains: [
+      'localhost',
+      'icon-library.com',
+      'image.tmdb.org',
+      process.env.hostname
+    ]
   }
 }
